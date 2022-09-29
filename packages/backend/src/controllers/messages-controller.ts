f73859e39aff8ff6669444ced759229c4bced1d5
Mattias Söderberg <mattes.email@gmail.com>
2022-09-29 11:20:59 +0200

    Add get messages route

--- packages/backend/src/controllers/messages-controller.ts
+++ packages/backend/src/controllers/messages-controller.ts
@@ -1,7 +1,10 @@
 import { Router, Request, Response } from 'express'
+import { loadMessages } from '../services/messages-services'
 
 const messagesRouter = Router()
 
-messagesRouter.get("/", (req: Request, res: Response) => {
-    
+messagesRouter.get("/", async (req: Request, res: Response) => {
+    res.send(await loadMessages())
 })
+
+export default messagesRouter
