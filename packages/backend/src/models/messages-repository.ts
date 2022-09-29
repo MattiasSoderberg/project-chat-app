55ef91be1d4c43b2c1ec671c2bb01f15966c479b
Mattias Söderberg <mattes.email@gmail.com>
2022-09-29 11:20:47 +0200

    Add export to all functions

--- packages/backend/src/models/messages-repository.ts
+++ packages/backend/src/models/messages-repository.ts
@@ -1,25 +1,25 @@
 import { pool } from '../db'
 import { sql } from 'slonik'
 
-const getAllMessages = async () => {
+export const getAllMessages = async () => {
     return (await pool).connect(async (connection) => {
         return await connection.query(sql`SELECT * FROM messages`)
     })
 }
 
-const getAllMessagesNotByUser = async (authorId: number) => {
+export const getAllMessagesNotByUser = async (authorId: number) => {
     return (await pool).connect(async (connection) => {
         return await connection.query(sql`SELECT * FROM messages WHERE id IS NOT authorId`)
     })
 }
 
-const getMessagesByUserId = async (authorId: number) => {
+export const getMessagesByUserId = async (authorId: number) => {
     return (await pool).connect(async (connection) => {
         return await connection.query(sql`SELECT * FROM messages WHERE author=${authorId}`)
     })
 }
 
-const createMessage = async (text: string, authorId: number) => {
+export const createMessage = async (text: string, authorId: number) => {
     return (await pool).connect(async (connection) => {
         return await connection.query(sql`INSERT INTO messages (text, author)
         VALUES (${text}, ${authorId})`)
