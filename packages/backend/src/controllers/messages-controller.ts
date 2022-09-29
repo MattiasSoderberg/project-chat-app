ebdd4977681b72502aa4f61fe0b346340d414bb9
Mattias Söderberg <mattes.email@gmail.com>
2022-09-29 17:00:16 +0200

    Add post route

--- packages/backend/src/controllers/messages-controller.ts
+++ packages/backend/src/controllers/messages-controller.ts
@@ -1,10 +1,18 @@
 import { Router, Request, Response } from 'express'
-import { loadMessages } from '../services/messages-services'
+import { loadMessages, saveMessage } from '../services/messages-services'
+import { auth, CustomRequest } from '../middlewares/auth'
 
 const messagesRouter = Router()
 
-messagesRouter.get("/", async (req: Request, res: Response) => {
+messagesRouter.get("/", auth, async (req: Request, res: Response) => {
+    const user = (req as CustomRequest).user
+    // console.log(user.id)
     res.send(await loadMessages())
+})
+
+messagesRouter.post("/", auth, async (req: Request, res: Response) => {
+    const username = String((req as CustomRequest).user)
+    res.send(await saveMessage(req.body.text, username))
 })
 
 export default messagesRouter
--- packages/backend/src/services/messages-services.ts
+++ packages/backend/src/services/messages-services.ts
@@ -1,6 +1,14 @@
 // import { MessageItem } from '@chat-app/shared'
-import { getAllMessages } from '../models/messages-repository'
+import { createMessage, getAllMessages } from '../models/messages-repository'
+import { getUserByUsername } from '../models/users-repository'
 
 export const loadMessages = async () => {
     return await getAllMessages()
+}
+
+export const saveMessage = async (text: string, username: string) => {
+    const user = await getUserByUsername(username)
+    const userId = Number(user.rows[0].id)
+    await createMessage(text, userId)
+    return await loadMessages()
 }
