d35c9e7f2a4a1f46895590b396ab36cf2469070a
Mattias Söderberg <mattes.email@gmail.com>
2022-10-02 16:47:11 +0200

    Remove user context

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,17 +1,13 @@
-import React, { useState, createContext } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import { Box, Heading } from '@chakra-ui/react'
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ChatPage from './pages/ChatPage';
 
-const UserContext = createContext({})
-
 function App() {
-  const [user, setUser] = useState({})
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
       <Box>
         <Heading as='h1' size='2xl'>Chat-App</Heading>
 
@@ -21,7 +17,6 @@ function App() {
           <Route path='/chat' element={<ChatPage />} />
         </Routes>
       </Box>
-    </UserContext.Provider>
   );
 }
 
