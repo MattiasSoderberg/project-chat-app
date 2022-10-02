37431b494e0b4fb3b05e553ddcdee5d4b93b87c5
Mattias Söderberg <mattes.email@gmail.com>
2022-10-02 16:48:12 +0200

    Add send message, log out, fetch user and refactor

--- packages/frontend/src/pages/ChatPage.tsx
+++ packages/frontend/src/pages/ChatPage.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import {
     Heading,
@@ -9,46 +10,88 @@ import Message from '../components/Message'
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
-const fetchMessages = async () => {
-    const token = localStorage.getItem('chat-app-token')
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
+type messageItem = {
+    id: number,
+    text: string,
+    username: string
+}
+
+type userType = {
+    username: string,
+    id: number
+}
+
+const config = {
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem('chat-app-token')}`
     }
+}
+
+const fetchMessages = async () => {
     const response = await axios.get('/messages', config)
     // console.log(response.data.rows[0].text)
     return response.data.rows
 }
 
+const fetchUser = async () => {
+    const response = await axios.get('/users/me', config)
+    return response.data
+}
+
 export default function ChatPage() {
-    const [messageList, setMessageList] = useState<{ id: number, text: string, username: string }[]>([])
+    const [messageList, setMessageList] = useState<messageItem[]>([])
     const [error, setError] = useState('')
     const [inputText, setInputText] = useState('')
+    const [user, setUser] = useState<userType>({username: '', id: 0})
+    const navigate = useNavigate()
 
-    // const sendMessage = () => {
-    //     const message = {
-    //         text: inputText,
-    //         author: 
-    //     }
-    //     axios.post('/messages')
-    // }
+
+    const sendMessage = async (text: string, authorId: number): Promise<void>=> {
+        const payload = {
+            text: text,
+            author: authorId
+        }
+        console.log(payload, config)
+        await axios.post('/messages', payload, config)
+        // const messages = await fetchMessages()
+        setMessageList(await fetchMessages())
+
+    }
+
+    const handleLogout = () => {
+        localStorage.removeItem('chat-app-token')
+        setUser({username: '', id: 0})
+        navigate('/')
+    }
 
     useEffect(() => {
+        fetchUser()
+            .then(setUser)
+            .catch(err => {
+                setError('Coldn´t fetch user')
+            })
+    }, [])
+
+    useEffect(() => {
+        console.log('useeffect', user)
         fetchMessages()
             .then(setMessageList)
             .catch(err => {
                 setMessageList([])
                 setError('Couldn´t fetch messages...')
             })
-    }, [])
+    }, [user])
+
 
     return (
         <div>
             <Heading>Chat</Heading>
             {messageList.length > 0 && messageList.map(message => {
-                return <Message username={message.username} text={message.text} />
+                return <Message key={message.id} username={message.username} text={message.text} />
             })}
+            <Input value={inputText} onChange={e => setInputText(e.target.value)} />
+            <Button onClick={e => sendMessage(inputText, user.id)}>Send</Button>
+            <Button onClick={handleLogout}>Log Out</Button>
         </div>
     )
 }
