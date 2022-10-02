abad1681166eab582d5d563f7035a30aacdd5317
Mattias Söderberg <mattes.email@gmail.com>
2022-10-02 15:27:00 +0200

    Add signup page

--- packages/frontend/src/pages/SignupPage.tsx
+++ packages/frontend/src/pages/SignupPage.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import { Heading } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { UserCredentials } from '@chat-app/shared'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import {
+    Heading,
+    Input,
+    Button,
+} from '@chakra-ui/react'
 
-export default function SignupPage() {
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+
+export default function ChatPage() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const createAccount = async (username: string, password: string): Promise<void> => {
+        const userCredentials: UserCredentials = {
+            username, 
+            password
+        }
+        try {
+            const response = await axios.post('/users', userCredentials)
+            localStorage.setItem('chat-app-token', response.data.token)
+            navigate('/')
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div>
-            <Heading>SignupPage</Heading>
+            <Heading as='h2'>Sign Up Page</Heading>
+            <form>
+                <Input type='text' placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} />
+                <Input type='password' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} />
+                <Button onClick={e => createAccount(username, password)}>Sign Up</Button>
+            </form>
         </div>
     )
 }
