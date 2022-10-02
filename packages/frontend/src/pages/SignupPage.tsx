37a95acd7ef403a6e85dab8a3d77fa111d3d8fe8
Mattias Söderberg <mattes.email@gmail.com>
2022-10-02 13:52:45 +0200

    Add signup page

---
+++ packages/frontend/src/pages/SignupPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Heading } from '@chakra-ui/react'
+
+export default function SignupPage() {
+    return (
+        <div>
+            <Heading>SignupPage</Heading>
+        </div>
+    )
+}
