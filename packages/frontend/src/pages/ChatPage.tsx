d655753502b82ff14f09bac233c63de44672c063
Mattias Söderberg <mattes.email@gmail.com>
2022-10-02 13:52:37 +0200

    Add chat page

---
+++ packages/frontend/src/pages/ChatPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Heading } from '@chakra-ui/react'
+
+export default function ChatPage() {
+    return (
+        <div>
+            <Heading>ChatPage</Heading>
+        </div>
+    )
+}
