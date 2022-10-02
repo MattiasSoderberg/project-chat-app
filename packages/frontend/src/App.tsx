36e676569494f9a09698a556eb39f94df9975588
Mattias Söderberg <mattes.email@gmail.com>
2022-10-02 15:27:25 +0200

    Add user context

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import { Box, Heading } from '@chakra-ui/react'
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ChatPage from './pages/ChatPage';
 
-function App() {
-  return (
-    <Box>
-      <Heading>Chat-App</Heading>
+const UserContext = createContext({})
 
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='/chat' element={<ChatPage />} />
-      </Routes>
-    </Box>
+function App() {
+  const [user, setUser] = useState({})
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Box>
+        <Heading as='h1' size='2xl'>Chat-App</Heading>
+
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+          <Route path='/chat' element={<ChatPage />} />
+        </Routes>
+      </Box>
+    </UserContext.Provider>
   );
 }
 
