273c0e0c8c808220140374d9e28abfdc9d8b627d
Mattias Söderberg <mattes.email@gmail.com>
2022-10-02 12:25:53 +0200

    Clean up remove console.logs

--- packages/backend/src/controllers/messages-controller.ts
+++ packages/backend/src/controllers/messages-controller.ts
@@ -1,18 +1,15 @@
 import { Router, Request, Response } from 'express'
 import { loadMessages, saveMessage } from '../services/messages-services'
-import { auth, CustomRequest } from '../middlewares/auth'
+import { auth } from '../middlewares/auth'
 
 const messagesRouter = Router()
 
 messagesRouter.get("/", auth, async (req: Request, res: Response) => {
-    const user = (req as CustomRequest).user
-    // console.log(user.id)
     res.send(await loadMessages())
 })
 
 messagesRouter.post("/", auth, async (req: Request, res: Response) => {
-    const username = String((req as CustomRequest).user)
-    res.send(await saveMessage(req.body.text, username))
+    res.send(await saveMessage(req.body.text, String(req.user)))
 })
 
 export default messagesRouter
