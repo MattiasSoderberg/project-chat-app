e4bf9ef25f0330758f0a694672df15b41fe2557c
Mattias Söderberg <mattes.email@gmail.com>
2022-10-02 13:52:25 +0200

    Add more routes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import { Box, Heading } from '@chakra-ui/react'
 import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import ChatPage from './pages/ChatPage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
 
       <Routes>
         <Route path='/' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/chat' element={<ChatPage />} />
       </Routes>
     </Box>
   );
