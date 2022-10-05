23e3e97dc606c3ba85082f0354cfe9f67142071f
Mattias Söderberg <mattes.email@gmail.com>
2022-10-05 12:27:01 +0200

    Fix crash when wrong username

--- packages/backend/src/services/users-services.ts
+++ packages/backend/src/services/users-services.ts
@@ -7,24 +7,23 @@ export const saveNewUser = async (userCredentials: UserCredentials) => {
     const { username, password } = userCredentials
 
     const hashedPassword = await hash(password, 8)
+    const date = new Date().toISOString()
     
-    return await createUser(username, hashedPassword)
+    return await createUser(username, hashedPassword, date)
     
-}
+} 
 
 export const loginUser = async (userCredetials: UserCredentials) => { // TODO fix crash when username and password are empty
     const { username, password } = userCredetials
 
     const user = await getUserByUsername(username)
-    const hashedPassword = String(user.rows[0].password)
-
-    if (user) {
+    
+    if (user.rows[0]) {
+        const hashedPassword = String(user.rows[0].password)
         if (await compare(password, hashedPassword)) {
             return sign({username: user.rows[0].username, id: user.rows[0].id}, String(process.env.JWT_SECRET), { expiresIn: '1800s' })
         }
     }
-
-    return
 }
 
 export const loadLoggedInUser = async (user: JwtPayload) => {
