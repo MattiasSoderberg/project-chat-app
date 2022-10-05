7ad47acb5cdfc0b3c225c3887e53af21bff898f5
Mattias Söderberg <mattes.email@gmail.com>
2022-10-05 12:27:36 +0200

    Add error response when wrong credentials

--- packages/backend/src/controllers/users-controller.ts
+++ packages/backend/src/controllers/users-controller.ts
@@ -22,7 +22,11 @@ usersRouter.post("/", async (req: Request<UserCredentials>, res: Response) => {
 
 usersRouter.post("/login", async (req: Request<UserCredentials>, res: Response) => {
     const token = await loginUser(req.body)
-    res.status(200).json({token})
+    if (token) {
+        res.status(200).json({token})
+    } else {
+        res.status(400).json({ message: 'Incorrect username or password'})
+    }
 })
 
 usersRouter.get("/me", auth, async (req: Request, res: Response) => {
