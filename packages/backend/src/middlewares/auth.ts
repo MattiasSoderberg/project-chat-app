8dd1a6aac0fb999236452253d612609395cde775
Mattias Söderberg <mattes.email@gmail.com>
2022-10-07 20:05:13 +0200

    Fix response when empty header

--- packages/backend/src/middlewares/auth.ts
+++ packages/backend/src/middlewares/auth.ts
@@ -6,10 +6,14 @@ const JWT_SECRET: Secret = String(process.env.JWT_SECRET)
 export const auth = (req: Request, res: Response, next: NextFunction) => {
     try {
         const authHeader = req.header('Authorization')
-
+        
+        if (!authHeader) {
+            res.status(401).json({ message: 'Invalid header'})
+        }
+        
         if (authHeader && authHeader.split(' ')[0] === 'Bearer') {
             const token = authHeader.split(' ')[1]
-
+            
             if (token) {
                 req.user = verify(token, JWT_SECRET) as JwtPayload
                 next()
