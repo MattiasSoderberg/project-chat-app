8f596f9f437f12042efa15623a79aa6af299127d
Mattias Söderberg <mattes.email@gmail.com>
2022-10-07 20:04:06 +0200

    Add get and create functions and slug to table

--- packages/backend/src/models/rooms-repository.ts
+++ packages/backend/src/models/rooms-repository.ts
@@ -1,5 +1,5 @@
 import { pool } from './db'
-import { sql } from 'slonik'
+import { sql, UniqueIntegrityConstraintViolationError } from 'slonik'
 
 
 export const createRoomsTable = async () => {
@@ -8,11 +8,35 @@ export const createRoomsTable = async () => {
         CREATE TABLE IF NOT EXISTS rooms (
             id serial PRIMARY KEY,
             title VARCHAR NOT NULL UNIQUE,
+            slug VARCHAR NOT NULL UNIQUE,
             owner VARCHAR NOT NULL,
             FOREIGN KEY (owner)
                 REFERENCES users (username)
                 ON DELETE CASCADE
         )
         `)
+    })
+}
+
+export const getAllRooms = async () => {
+    return (await pool).connect(async connection => {
+        return await connection.any(sql`SELECT * FROM rooms`)
+    })
+}
+
+export const createRoom = async (title: string, owner: string) => {
+    return (await pool).connect(async connection => {
+        try {
+            return await connection.query(sql`
+                INSERT INTO rooms (title, owner)
+                VALUES (${title}, ${owner})
+            `)
+        } catch (err) {
+            if (err instanceof UniqueIntegrityConstraintViolationError) {
+                console.error(err.message)
+            } else {
+                console.error(err)
+            }
+        }
     })
 }
