8648973b0af3e4b74f15b08d7863f1fe49113feb
Mattias Söderberg <mattes.email@gmail.com>
2022-10-12 15:44:51 +0200

    Add get by room route

--- packages/backend/src/controllers/messages-controller.ts
+++ packages/backend/src/controllers/messages-controller.ts
@@ -1,21 +1,30 @@
-import { Router, Request, Response } from 'express'
-import { loadMessages, saveMessage } from '../services/messages-services'
-import { auth } from '../middlewares/auth'
-import { JwtPayload } from 'jsonwebtoken'
+import { Router, Request, Response } from "express";
+import {
+  loadMessages,
+  loadMessagesByRoom,
+  saveMessage,
+} from "../services/messages-services";
+import { auth } from "../middlewares/auth";
+import { JwtPayload } from "jsonwebtoken";
 
-const messagesRouter = Router()
+const messagesRouter = Router();
 
 messagesRouter.get("/", auth, async (req: Request, res: Response) => {
-    res.send(await loadMessages())
-})
+  res.send(await loadMessages());
+});
 
 messagesRouter.post("/", auth, async (req: Request, res: Response) => {
-    const newMessage = (await saveMessage(req.body, req.user as JwtPayload))
-    if (newMessage) {
-        res.sendStatus(201)
-    } else {
-        res.status(400).json({ message: 'Could not create message' })
-    }
-})
+  const newMessage = await saveMessage(req.body, req.user as JwtPayload);
+  if (newMessage) {
+    res.sendStatus(201);
+  } else {
+    res.status(400).json({ message: "Could not create message" });
+  }
+});
 
-export default messagesRouter
+messagesRouter.get("/:roomId", auth, async (req: Request, res: Response) => {
+  const { roomId } = req.params;
+  res.send(await loadMessagesByRoom(parseInt(roomId)));
+});
+
+export default messagesRouter;
