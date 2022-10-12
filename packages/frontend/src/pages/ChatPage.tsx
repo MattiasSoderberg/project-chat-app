a3ca12d839066556b9446dae0edd9a7405d47436
Mattias Söderberg <mattes.email@gmail.com>
2022-10-12 15:51:45 +0200

    Refactor code into other components

--- packages/frontend/src/pages/ChatPage.tsx
+++ packages/frontend/src/pages/ChatPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link, useLocation } from 'react-router-dom'
 import axios from 'axios'
 import {
     Heading,
@@ -10,7 +10,8 @@ import {
     Flex,
     Spacer
 } from '@chakra-ui/react'
-import Message from '../components/Message'
+import { MessageItem } from '@chat-app/shared'
+import MessageList from '../components/MessageList'
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
 axios.interceptors.request.use(config => {
@@ -24,39 +25,23 @@ axios.interceptors.request.use(config => {
     return config
 })
 
-type messageItem = {
-    id: number,
-    text: string,
-    username: string
-}
-
-type userType = {
-    username: string,
-    id: number
-}
-
-const fetchMessages = async () => {
+const fetchMessages = async (): Promise<MessageItem[]> => {
     const response = await axios.get('/messages')
-    return response.data.rows
-}
-
-const fetchUser = async () => {
-    const response = await axios.get('/users/me')
     return response.data
 }
 
 export default function ChatPage() {
-    const [messageList, setMessageList] = useState<messageItem[]>([])
+    const [messageList, setMessageList] = useState<MessageItem[]>([])
     const [error, setError] = useState('')
     const [inputText, setInputText] = useState('')
-    const [user, setUser] = useState<userType>({ username: '', id: 0 })
     const navigate = useNavigate()
+    const location = useLocation()
 
-
-    const sendMessage = async (text: string, authorId: number): Promise<void> => {
+    const sendMessage = async (text: string, authorId: number, roomId: number): Promise<void> => {
         const payload = {
             text: text,
-            author: authorId
+            author: authorId,
+            room: roomId
         }
         setInputText('')
         await axios.post('/messages', payload)
@@ -66,17 +51,8 @@ export default function ChatPage() {
 
     const handleLogout = () => {
         localStorage.removeItem('chat-app-token')
-        setUser({ username: '', id: 0 })
         navigate('/')
     }
-
-    useEffect(() => {
-        fetchUser()
-            .then(setUser)
-            .catch(err => {
-                setError("Couldn't fetch user")
-            })
-    }, [])
 
     useEffect(() => {
         fetchMessages()
@@ -92,12 +68,11 @@ export default function ChatPage() {
         <Box p={10}>
             <Heading>Chat</Heading>
             <Flex direction='column' gap={2}>
-                {messageList.length > 0 ? messageList.map(message => {
-                    return <Message key={message.id} username={message.username} text={message.text} loggedInUser={user.username} />
-                }) : <Text>Loading messages...</Text>}
+                <MessageList messages={messageList} user={location.state.user} />
             </Flex>
                 <Input value={inputText} onChange={e => setInputText(e.target.value)} />
-                <Button onClick={e => sendMessage(inputText, user.id)}>Send</Button>
+                <Button onClick={e => sendMessage(inputText, location.state.user.id, location.state.room.id)}>Send</Button>
+                <Button as={Link} to='/home'>Home</Button>
                 <Button onClick={handleLogout}>Log Out</Button>
         </Box>
     )
