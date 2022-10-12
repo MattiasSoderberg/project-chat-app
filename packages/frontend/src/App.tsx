6a8505071ab2b7596ed2dc811d8da7c26e2fd981
Mattias Söderberg <mattes.email@gmail.com>
2022-10-12 15:49:15 +0200

    Add home path and added slug param to chat

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Box, Heading } from '@chakra-ui/react'
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ChatPage from './pages/ChatPage';
+import HomePage from './pages/HomePage';
 
 function App() {
 
@@ -14,7 +15,8 @@ function App() {
         <Routes>
           <Route path='/' element={<LoginPage />} />
           <Route path='/signup' element={<SignupPage />} />
-          <Route path='/chat' element={<ChatPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/chat/:slug' element={<ChatPage />} />
         </Routes>
       </Box>
   );
