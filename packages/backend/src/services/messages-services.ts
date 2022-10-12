8aac6fce98983085f3dad7f94d6fd77ac959ddb6
Mattias Söderberg <mattes.email@gmail.com>
2022-10-12 15:47:51 +0200

    Add loadMessagesByRoom

--- packages/backend/src/services/messages-services.ts
+++ packages/backend/src/services/messages-services.ts
@@ -1,10 +1,14 @@
 // import { MessageItem } from '@chat-app/shared'
 import { JwtPayload } from 'jsonwebtoken'
-import { createMessage, getAllMessages } from '../models/messages-repository'
+import { createMessage, getAllMessages, getMessageByRoom } from '../models/messages-repository'
 import { getUserByUsername } from '../models/users-repository'
 
 export const loadMessages = async () => {
     return await getAllMessages()
+}
+
+export const loadMessagesByRoom = async (room: number) => {
+    return await getMessageByRoom(room)
 }
 
 export const saveMessage = async (body: {text: string, room: number}, user: JwtPayload) => {
