1e65ed26eda5cbc14f08fe0f6a87be7e9824926d
Mattias Söderberg <mattes.email@gmail.com>
2022-10-12 15:48:31 +0200

    Add loadRoomById function

--- packages/backend/src/services/rooms-services.ts
+++ packages/backend/src/services/rooms-services.ts
@@ -1,12 +1,15 @@
 import { JwtPayload } from "jsonwebtoken";
 import { createSlug } from "../middlewares/helpers";
-import { getAllRooms, createRoom } from "../models/rooms-repository";
+import { getAllRooms, createRoom, getRoomById } from "../models/rooms-repository";
 
 export const loadAllRooms = async () => {
-    console.log('rooms services')
     return await getAllRooms()
 }
 
 export const saveNewRoom = async (body: {title: string}, user: JwtPayload) => {
     return await createRoom(body.title, user.username, createSlug(body.title))
+}
+
+export const loadRoomById = async (id: number) => {
+    return await getRoomById(id)
 }
