33d286b1af03d32be0c8c754feaaaa782bd99e06
Mattias Söderberg <mattes.email@gmail.com>
2022-10-15 11:40:46 +0200

    Add create new room modal

--- packages/frontend/src/components/RoomModal.tsx
+++ packages/frontend/src/components/RoomModal.tsx
@@ -1,15 +1,52 @@
 import React, { useState } from "react";
-import { Box, Button, Center, Heading, Input } from "@chakra-ui/react";
+import axios from 'axios'
+import { Box, Button, Center, Heading, Input, Text, useAccordion } from "@chakra-ui/react";
+import { RoomItem } from "@chat-app/shared";
 
-export default function RoomModal(props: { username: string }) {
-  const [title, setTitle] = useState("");
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.interceptors.request.use((config) => {
+  if (!config?.headers) {
+    config.headers = {};
+  }
+  const token = localStorage.getItem("chat-app-token");
+  if (token) {
+    config.headers["authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default function RoomModal(props: { username: string, 
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>,
+    setRooms: React.Dispatch<React.SetStateAction<RoomItem[]>>,
+    fetchRooms: any // Fix to something more right than any
+}) {
+  const [title, setTitle] = useState('');
+  const [error, setError] = useState('')
+
+  const createRoom = async (title: string) => {
+    const payload = {
+        title,
+        username: props.username
+    }
+    try {
+        await axios.post('/rooms', payload)
+        props.setShowModal(false)
+        props.setRooms(await props.fetchRooms())
+    } catch (err: any) {
+        if (axios.isAxiosError(err)) {
+            setError(err.response?.data as string)
+        }
+    }
+
+  }
 
   return (
     <Center width='100vw' height='100vh' bg='#00000080'>
       <Box width={400} height={300} bg='gray.200' p={10}>
         <Heading as='h3' mb={5}>New Room</Heading>
         <Input value={title} onChange={e => setTitle(e.target.value)} placeholder='Room Title' bg='gray.100' mb={5} />
-        <Button>Create Room</Button>
+        <Button onClick={e => createRoom(title)} mb={5}>Create Room</Button>
+        {error && <Text color='red.600'>{error}</Text>}
       </Box>
     </Center>
   );
