2a5e1831b8e121c00473e3a8a1c44b8a818963b6
Mattias Söderberg <mattes.email@gmail.com>
2022-10-16 13:29:30 +0200

    Refactor Message component

--- packages/frontend/src/components/MessageList.tsx
+++ packages/frontend/src/components/MessageList.tsx
@@ -1,33 +1,36 @@
-import { MessageItem } from '@chat-app/shared'
-import React from 'react'
-import {
-    Heading,
-    Text,
-    Flex
-} from '@chakra-ui/react'
+import { MessageItem } from "@chat-app/shared";
+import React from "react";
+import { Heading, Text, Flex, HStack } from "@chakra-ui/react";
 
-function Message (props: {message: MessageItem, loggedInUser: string}) {
-    return (
-        <>
-            {props.message.username === props.loggedInUser ?
-                <Flex direction='column' alignItems='end' width="100%">
-                    <Heading as='h3' size='md'>{props.message.username}</Heading>
-                    <Text>{props.message.text}</Text>
-                </Flex>
-                : <Flex direction='column' width="100%">
-                    <Heading as='h3' size='md'>{props.message.username}</Heading>
-                    <Text>{props.message.text}</Text>
-                </Flex>}
-        </>
-    )
+function Message(props: { message: MessageItem; loggedInUser: string }) {
+  return (
+    <Flex direction="column" width="100%">
+      <HStack>
+        <Heading as="h3" size="md">
+          {props.message.author}
+        </Heading>
+        <Text>{props.message.created_at}</Text>
+      </HStack>
+      <Text>{props.message.text}</Text>
+    </Flex>
+  );
 }
 
-export default function MessageList(props: { messages: MessageItem[], user: {id: number, username: string}}) {
+export default function MessageList(props: {
+  messages: MessageItem[];
+  user: { id: number; username: string };
+}) {
   return (
     <>
-        {props.messages.map(message => {
-            return <Message key={message.id} message={message} loggedInUser={props.user.username} />
-        })}
+      {props.messages.map((message) => {
+        return (
+          <Message
+            key={message.id}
+            message={message}
+            loggedInUser={props.user.username}
+          />
+        );
+      })}
     </>
-  )
+  );
 }
