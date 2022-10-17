4f359560d71a076d3cf5c98c72fc45b0eb1fff38
Mattias Söderberg <mattes.email@gmail.com>
2022-10-17 22:33:20 +0200

    Add Room modal for creating new room

--- packages/frontend/src/components/RoomModal.tsx
+++ packages/frontend/src/components/RoomModal.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
-import axios from 'axios'
-import { Box, Button, Center, Heading, Input, Text, useAccordion } from "@chakra-ui/react";
-import { RoomItem } from "@chat-app/shared";
+import axios from "axios";
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  Input,
+  Text,
+  HStack,
+  Spacer,
+} from "@chakra-ui/react";
+import { RoomItem, ServerItem } from "@chat-app/shared";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.interceptors.request.use((config) => {
@@ -15,38 +24,52 @@ axios.interceptors.request.use((config) => {
   return config;
 });
 
-export default function RoomModal(props: { username: string, 
-    setShowModal: React.Dispatch<React.SetStateAction<boolean>>,
-    setRooms: React.Dispatch<React.SetStateAction<RoomItem[]>>,
-    fetchRooms: any // Fix to something more right than any
+export default function RoomModal(props: {
+  server: ServerItem;
+  setShowRoomModal: React.Dispatch<React.SetStateAction<boolean>>;
+  setRooms: React.Dispatch<React.SetStateAction<RoomItem[]>>;
+  fetchRoomsOnServer: any; // Fix to something more right than any
 }) {
-  const [title, setTitle] = useState('');
-  const [error, setError] = useState('')
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState("");
 
   const createRoom = async (title: string) => {
     const payload = {
-        title,
-        username: props.username
-    }
+      title,
+      serverId: props.server.id as number,
+    };
     try {
-        await axios.post('/rooms', payload)
-        props.setShowModal(false)
-        props.setRooms(await props.fetchRooms())
+      await axios.post("/rooms", payload);
+      props.setShowRoomModal(false);
+      props.setRooms(await props.fetchRoomsOnServer(payload.serverId))
     } catch (err: any) {
-        if (axios.isAxiosError(err)) {
-            setError(err.response?.data as string)
-        }
+      if (axios.isAxiosError(err)) {
+        setError(err.response?.data as string);
+      }
     }
-
-  }
+  };
 
   return (
-    <Center width='100vw' height='100vh' bg='#00000080'>
-      <Box width={400} height={300} bg='gray.200' p={10}>
-        <Heading as='h3' mb={5}>New Room</Heading>
-        <Input value={title} onChange={e => setTitle(e.target.value)} placeholder='Room Title' bg='gray.100' mb={5} />
-        <Button onClick={e => createRoom(title)} mb={5}>Create Room</Button>
-        {error && <Text color='red.600'>{error}</Text>}
+    <Center width="100vw" height="100vh" bg="#00000095" position="absolute" top={0} left={0} zIndex={10}>
+      <Box width={400} height={300} bg="gray.200" p={10}>
+        <Heading as="h3" mb={5}>
+          New Room
+        </Heading>
+        <Input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Room Title"
+          bg="gray.100"
+          mb={5}
+        />
+        <HStack mb={5}>
+          <Button onClick={(e) => createRoom(title)}>
+            Create Room
+          </Button>
+          <Spacer />
+          <Button onClick={(e) => props.setShowRoomModal(false)}>Close</Button>
+        </HStack>
+        {error && <Text color="red.600">{error}</Text>}
       </Box>
     </Center>
   );
