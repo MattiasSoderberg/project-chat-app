997e2c0df3590360cacda6553386fdefad0d3302
Mattias Söderberg <mattes.email@gmail.com>
2022-10-17 22:46:11 +0200

    Remove ChatPage

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 import { Box, Heading } from '@chakra-ui/react'
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-import ChatPage from './pages/ChatPage';
 import HomePage from './pages/HomePage';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
           <Route path='/' element={<LoginPage />} />
           <Route path='/signup' element={<SignupPage />} />
           <Route path='/home' element={<HomePage />} />
-          <Route path='/chat/:slug' element={<ChatPage />} />
         </Routes>
       </Box>
   );
