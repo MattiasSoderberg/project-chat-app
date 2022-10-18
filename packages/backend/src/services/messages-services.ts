deec305d6d1dcf78c5102f35dce6e8b2e7b2cfb0
Mattias Söderberg <mattes.email@gmail.com>
2022-10-18 22:38:49 +0200

    Remove unused import

--- packages/backend/src/services/messages-services.ts
+++ packages/backend/src/services/messages-services.ts
@@ -1,7 +1,6 @@
 // import { MessageItem } from '@chat-app/shared'
 import { JwtPayload } from 'jsonwebtoken'
 import { createMessage, getAllMessages, getMessageByRoom } from '../models/messages-repository'
-import { getUserByUsername } from '../models/users-repository'
 
 export const loadMessages = async () => {
     return await getAllMessages()
