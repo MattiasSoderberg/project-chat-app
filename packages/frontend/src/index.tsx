b90c064c8b16233d886fc40e0d7c30e5f6c55b29
Mattias Söderberg <mattes.email@gmail.com>
2022-10-18 08:43:33 +0200

    Remove StrictMode

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
     <ChakraProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <App />
     </ChakraProvider>
   </BrowserRouter>
 );
