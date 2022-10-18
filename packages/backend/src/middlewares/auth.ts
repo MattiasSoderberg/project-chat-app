8e2d32985417549af08bcdf0adf5c9e28da32605
Mattias Söderberg <mattes.email@gmail.com>
2022-10-18 22:39:08 +0200

    Add socket auth middleware

--- packages/backend/src/middlewares/auth.ts
+++ packages/backend/src/middlewares/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import { verify, JwtPayload, Secret } from 'jsonwebtoken'
+import { Socket } from "socket.io"
 
 const JWT_SECRET: Secret = String(process.env.JWT_SECRET)
 
@@ -23,4 +24,23 @@ export const auth = (req: Request, res: Response, next: NextFunction) => {
         res.status(403).send('Invalid token')
     }
 
+}
+
+export const socketAuth = (socket: Socket, next: NextFunction) => {
+    const token: string = socket.handshake.auth.token
+    console.log(token)
+    if (token) {
+        try {
+            if (token) {
+                const user = verify(token, JWT_SECRET) as JwtPayload
+                socket.data.user = user
+                next()
+            }
+        } catch (err) {
+            console.error(err)
+            next(new Error("Socket Authentication error"))
+        }
+    } else {
+        next(new Error("Socket authentication error"))
+    }
 }
