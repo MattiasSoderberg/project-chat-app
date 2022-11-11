dabb2b2230531888a6b1196d3d7b938a6a6134e9
Mattias Söderberg <mattes.email@gmail.com>
2022-11-11 11:13:35 +0100

    Refactor code to container component

--- packages/frontend/src/components/MessageList.tsx
+++ packages/frontend/src/components/MessageList.tsx
@@ -2,17 +2,68 @@ import { MessageItem } from "@chat-app/shared";
 import React from "react";
 import { Heading, Text, Flex, HStack } from "@chakra-ui/react";
 
-function Message(props: { message: MessageItem; loggedInUser: string }) {
+function MessageContainer(props: { children: JSX.Element[]; bgColor: string }) {
   return (
-    <Flex direction="column" width="100%" color="white">
-      <HStack>
-        <Heading as="h3" size="sm">
-          {props.message.author}
-        </Heading>
-        <Text fontSize={12}>{props.message.created_at}</Text>
-      </HStack>
-      <Text>{props.message.text}</Text>
+    <Flex
+      direction="column"
+      width="100%"
+      color="white"
+      bg={props.bgColor}
+      p={3}
+      borderBottom="1px"
+      borderColor="gray.800"
+    >
+      {props.children}
     </Flex>
+  );
+}
+
+function Message(props: { message: MessageItem; loggedInUser: string }) {
+  const date = new Date(props.message.created_at as string);
+  const dateString = `${date.toLocaleDateString()} - ${date.toLocaleTimeString()}`;
+
+  return (
+    <>
+      {props.message.author.toString() !== props.loggedInUser.toString() ? (
+        <MessageContainer bgColor="gray.700">
+          <HStack>
+            {props.message.author.toString() !==
+            props.loggedInUser.toString() ? (
+              <Heading as="h3" size="sm" color="gray.100">
+                {props.message.author}
+              </Heading>
+            ) : (
+              <Heading as="h3" size="sm" color="gray.100">
+                {props.message.author}
+              </Heading>
+            )}
+            <Text fontSize={12} color="gray.400">
+              {dateString}
+            </Text>
+          </HStack>
+          <Text>{props.message.text}</Text>
+        </MessageContainer>
+      ) : (
+        <MessageContainer bgColor="gray.600">
+          <HStack>
+            {props.message.author.toString() !==
+            props.loggedInUser.toString() ? (
+              <Heading as="h3" size="sm">
+                {props.message.author}
+              </Heading>
+            ) : (
+              <Heading as="h3" size="sm" color="blue.500">
+                {props.message.author}
+              </Heading>
+            )}
+            <Text fontSize={12} color="gray.400">
+              {dateString}
+            </Text>
+          </HStack>
+          <Text>{props.message.text}</Text>
+        </MessageContainer>
+      )}
+    </>
   );
 }
 
@@ -24,7 +75,7 @@ export default function MessageList(props: {
     <>
       {props.messages.map((message) => {
         return (
-          <Message 
+          <Message
             key={message.id}
             message={message}
             loggedInUser={props.user.username}
