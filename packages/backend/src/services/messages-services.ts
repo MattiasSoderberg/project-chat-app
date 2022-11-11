0350025f2a5d88b0891d23a406e075f780f09d9f
Mattias Söderberg <mattes.email@gmail.com>
2022-11-11 11:18:24 +0100

    Type username to string

--- packages/backend/src/services/messages-services.ts
+++ packages/backend/src/services/messages-services.ts
@@ -1,20 +1,33 @@
 // import { MessageItem } from '@chat-app/shared'
-import { JwtPayload } from 'jsonwebtoken'
-import { createMessage, getAllMessages, getMessageById, getMessagesByRoom } from '../models/messages-repository'
+import { JwtPayload } from "jsonwebtoken";
+import {
+  createMessage,
+  getAllMessages,
+  getMessageById,
+  getMessagesByRoom,
+} from "../models/messages-repository";
 
 export const loadMessages = async () => {
-    return await getAllMessages()
-}
+  return await getAllMessages();
+};
 
 export const loadMessagesByRoom = async (room: number) => {
-    return await getMessagesByRoom(room)
-}
+  return await getMessagesByRoom(room);
+};
 
 export const loadMessageById = async (id: number) => {
-    return await getMessageById(id)
-}
+  return await getMessageById(id);
+};
 
-export const saveMessage = async (body: {text: string, room: number}, user: JwtPayload) => {
-    const date = new Date().toISOString()
-    return await createMessage(body.text, user.username, date, body.room)
-}
+export const saveMessage = async (
+  body: { text: string; room: number },
+  user: JwtPayload
+) => {
+  const date = new Date().toISOString();
+  return await createMessage(
+    body.text,
+    user.username as string,
+    date,
+    body.room
+  );
+};
